27e8521a9c9c6869e73b42c413d444dad92253ed
Montana Mendy <montana@getprowl.com>
2021-04-21 00:34:18 -0700

    push build from vim

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ARCH=$(uname -m)
+git clone https://github.com/travis-ci/travis-build.git
+cd ./travis-build
+git checkout -t origin/enterprise-2.2
+rm -f ./Dockerfile
+cp ../Dockerfile.s390x ./Dockerfile
+docker build -t quay.io/hancockp/travis:travis-build-ee-s390x-20200818 .
+docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+docker images
+docker push quay.io/hancockp/travis:travis-build-ee-s390x-20200818
